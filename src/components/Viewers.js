6a73bfd1fb7e070df1770b4da1dcb5d619b5aea4
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-05-23 03:08:15 +0530

    ADD: Video Autoplay on Viewer

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,18 +6,36 @@ function Viewers() {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
       </Wrap>
     </Container>
   );
@@ -30,7 +48,12 @@ const Container = styled.div`
   display: grid;
   padding: 30px 0 26px;
   grid-gap: 25px;
+  gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
 `;
 
 const Wrap = styled.div`
@@ -40,11 +63,29 @@ const Wrap = styled.div`
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  overflow: hidden;
+  position: relative;
 
   img {
-    width: 100%;
+    inset: 0px;
+    display: block;
     height: 100%;
     object-fit: cover;
+    opacity: 1;
+    //position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    opacity: 0;
+    z-index: 0;
   }
 
   &:hover {
@@ -52,5 +93,9 @@ const Wrap = styled.div`
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
+
+    video {
+      opacity: 1;
+    }
   }
 `;
